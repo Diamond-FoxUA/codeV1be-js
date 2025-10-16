133b941fece697eab158f06bd7a1ea2f8db84e99
Dmytro Farbun <dimofarb@gmail.com>
2025-10-16 22:30:09 +0300

    changes to main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
+import './js/books-api.js';
+import './js/feedbacks.js';
+import './js/hero.js';
 import './js/events.js';
-import './js/footer.js';
+import './js/header.js';
+import './js/modal.js';
+import './js/book-modal.js';
