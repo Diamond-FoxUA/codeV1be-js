252cc279637f65f77a5bc28431ce037f15fdbca8
Svitwave <svitlanamaz@gmail.com>
2025-10-17 14:51:15 +0300

    add scroll-up

---
+++ src/js/scroll.js
@@ -0,0 +1,25 @@
+const scrollUpBtn = document.createElement('button');
+scrollUpBtn.id = 'scrollUp';
+scrollUpBtn.className = 'scroll-up';
+scrollUpBtn.innerHTML = `
+  <svg class="svg" width="30" height="30">
+    <use href="./img/icons.svg#icon-right-arrow-alt"></use>
+  </svg>
+`;
+
+document.body.appendChild(scrollUpBtn);
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollUpBtn.classList.add('show');
+  } else {
+    scrollUpBtn.classList.remove('show');
+  }
+});
+
+scrollUpBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
